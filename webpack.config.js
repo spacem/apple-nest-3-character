f0113032e2cb685f5e171d5d5213912d592a1bac
spacem <spacem>
2021-11-22 23:56:52 +1000

    move problimatic packages to externals

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ module.exports = (options, webpack) => {
   const lazyImports = [
     '@nestjs/microservices/microservices-module',
     '@nestjs/websockets/socket-module',
-    '@apollo/gateway',
-    'apollo-server-fastify',
-    'class-transformer/storage',
-    '@apollo/federation',
-    '@apollo/subgraph',
-    'ts-morph',
-    '@apollo/subgraph/dist/directives',
   ];
 
   return {
     ...options,
-    externals: [],
+    externals: [
+      '@apollo/gateway',
+      'apollo-server-fastify',
+      'class-transformer/storage',
+      '@apollo/federation',
+      '@apollo/subgraph',
+      'ts-morph',
+      '@apollo/subgraph/dist/directives',
+    ],
     plugins: [
       ...options.plugins,
       new webpack.IgnorePlugin({
