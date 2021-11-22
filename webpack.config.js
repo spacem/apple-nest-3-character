ffef672aaba9a3286843247864bd92bc6292d34d
spacem <spacem>
2021-11-22 23:52:14 +1000

    build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = (options, webpack) => {
+  const lazyImports = [
+    '@nestjs/microservices/microservices-module',
+    '@nestjs/websockets/socket-module',
+    '@apollo/gateway',
+    'apollo-server-fastify',
+    'class-transformer/storage',
+    '@apollo/federation',
+    '@apollo/subgraph',
+    'ts-morph',
+    '@apollo/subgraph/dist/directives',
+  ];
+
+  return {
+    ...options,
+    externals: [],
+    plugins: [
+      ...options.plugins,
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          if (lazyImports.includes(resource)) {
+            try {
+              require.resolve(resource);
+            } catch (err) {
+              return true;
+            }
+          }
+          return false;
+        },
+      }),
+    ],
+  };
+};
