d6865adbc293839db51fe2c94ce3f134c1dae74d
spacem <spacem>
2021-11-30 22:38:50 +1000

    Try to fix handler export

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { env } from './env';
 import { bootstrap } from './dev';
+import { Logger } from '@nestjs/common';
+import { handler } from './lambda';
+
+const logger = new Logger('main');
 
 if (env.PRODUCTION !== 'true') {
+  logger.log('Starting in dev mode');
   bootstrap();
+} else {
+  logger.log('Starting in prod mode');
 }
 
-export { handler } from './lambda';
+module.exports = handler;
